18164556d17e006d38e917ba2e4ee9447f9d0e6c
robert5313 <kiptoorobert@zetech.ac.ke>
2023-09-16 20:35:23 +0000

    tasks declarations

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Module for task declaration'''
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
